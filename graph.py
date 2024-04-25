a709c44e01595b597ced07393e850e3d97b3169f
“Sushrit <sushrit.k@gmail.com>
2024-04-25 10:11:36 -0500

    updated functions

--- graph.py
+++ graph.py
@@ -9,24 +9,55 @@ input_directory = 'split_files/name.basics'
 output_graph_file = 'graph.pickle'
 
 # Create a defaultdict to store the graph
-graph = defaultdict(list)
+graph_to_save = defaultdict(list)
 
+# Function to load the graph from a pickle file
+def title_to_pickle(input_directory):
+    title_data = {}
+    for filename in os.listdir(input_directory):
+            if filename.endswith('.tsv'):
+                with open(os.path.join(input_directory, filename), 'r', encoding='utf-8') as tsvfile:
+                    reader = csv.reader(tsvfile, delimiter='\t')
+                    next(reader)  # Skiping header
+                    for row in reader:
+                        # Extracting title ID and name from the 0th and 2nd elements
+                        title_id = row[0]
+                        title_type = row[2]
+                        title_data[title_id] = title_type
+    output_pickle_file = 'title.pickle'
+    with open(output_pickle_file, 'wb') as f:
+        pickle.dump(title_data, f)
+    print("Title data saved to title.pickle.")
+
+# Function to load the title data from a pickle file
+def load_title_from_pickle():
+    with open("title.pickle", 'rb') as f:
+        title = pickle.load(f)
+    return title
+    
 # Function to build the graph from split files
 def build_graph():
-    for filename in os.listdir(input_directory):
+    input_directory_title = 'split_files/title.basics'
+    title_to_pickle(input_directory_title)
+    #Loading the title data from the pickle file
+    title_data = load_title_from_pickle()
+    #Iterating through the name.basics files
+    for filename in os.listdir("split_files/name.basics"):
         if filename.endswith('.tsv'):
-            with open(os.path.join(input_directory, filename), 'r', encoding='utf-8') as tsvfile:
+            with open(os.path.join("split_files/name.basics", filename), 'r', encoding='utf-8') as tsvfile:
                 reader = csv.reader(tsvfile, delimiter='\t')
-                next(reader)  # Skip header
+                next(reader)  # Skiping header
                 for row in reader:
                     primary_profession = row[4]
-                    # Check if primaryProfession contains "actor" or "actress"
+                    # Checking if primaryProfession contains "actor" or "actress"
                     if "actor" in primary_profession.lower() or "actress" in primary_profession.lower():
-                        actor_id = row[1]
+                        actor = row[1]
                         known_for = row[5].split(',') if row[5] != '\\N' else []  # Split knownForTitles if not NULL
                         for movie in known_for:
-                            graph[actor_id].append(movie)
-                            graph[movie].append(actor_id)
+                            movie_name = title_data.get(movie, None)
+                            graph_to_save[actor].append(movie_name)
+                            graph_to_save[movie_name].append(actor)
+
 
 
 # Build the graph
@@ -35,7 +66,7 @@ build_graph()
 # Function to save the graph to a file
 def save_graph():
     with open(output_graph_file, 'wb') as f:
-        pickle.dump(graph, f)
+        pickle.dump(graph_to_save, f)
 
 # Save the graph
 save_graph()
--- main.py
+++ main.py
@@ -7,11 +7,6 @@ def load_graph_from_pickle(file_path):
         graph = pickle.load(f)
     return graph
 
-def load_title_from_pickle():
-    with open("title.pickle", 'rb') as f:
-        title = pickle.load(f)
-    return title
-
 # Define function to find shortest path using Dijkstra's algorithm
 def dijkstra(graph, start):
     distances = {node: float('inf') for node in graph}
@@ -49,41 +44,34 @@ def degree_of_separation(graph, source_actor, target_actor):
     degree = distances[target_actor]
     return degree, path
 
-def get_movie(movie_id):
-    title_data = load_title_from_pickle()
-    movies = []
-    for movie in movie_id:
-        movie_name = title_data.get(movie, None)
-        # print(movie_name)
-        movies.append(movie_name)
-    return movies
-
-
-def print_result(degree, path, source_actor, target_actor):
-    movie_names = get_movie(path)
-    print(f">> Degree of separation between {source_actor} and {target_actor} is {degree//2}.")
+# Function to print the result in correct format
+def print_result(degree, path):
+    l = len(path)
+    print(f">> Degree of separation between {path[0]} and {path[l-1]} is {l//2}.")
     for n in range(0, len(path) - 1, 2):
         actor_name = path[n]
-        movie_name = movie_names[n + 1]
+        movie_name = path[n + 1]
         next_actor_name = path[n + 2]
         print(f">> {actor_name} has starred with {next_actor_name} in the movie {movie_name}")
+    print()
 
 
 if __name__ == '__main__':
     # Load the graph from pickle file
     graph = load_graph_from_pickle('graph.pickle')
 
-    # Example usage
-    source_actor = 'Jim Carrey'
-    target_actor = 'Jeff Daniels'
+    source_actor = input("Enter the source actor: ")
+    target_actor = input("Enter the target actor: ")
+    print()
     result = degree_of_separation(graph, source_actor, target_actor)
     if isinstance(result, tuple) and len(result) == 2:
         degree, path = result
-        print_result(degree, path, source_actor, target_actor)
-    #     print(f"Degree of separation between {source_actor} and {target_actor}: {degree/2}")
-    #     if isinstance(path, list):
-    #         print("Path:")
-    #         for i in range(len(path) - 1):
-    #             print(f"{path[i]} - {path[i+1]}")
-    # else:
-    #     print(result)  # If result is not a tuple of length 2, print the result directly
+        if isinstance(result, tuple):
+            degree, path = result
+            if (len(path) % 2 != 0):
+                print_result(degree, path)
+            else:
+                print(f"Failed for {path[0]}. Even number of nodes in the path means incorrect path.")
+                print(path)
+        else:
+            print(result)
